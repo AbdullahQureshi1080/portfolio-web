fbc08a7011a5f891356db95ffa069ad8ce2c0d97
Abdullah Qureshi <55753562+AbdullahQureshi1080@users.noreply.github.com>
2020-06-22 18:41:07 +0500

    imageCorrectionUrl

--- js/script.js
+++ js/script.js
@@ -1,62 +1,4 @@
 
-//
-// var slideIndex = 0;
-// showSlides();
-// var slides,dots;
-//
-// function plusSlides(position) {
-//     slideIndex += position;
-//     if (slideIndex > slides.length) {slideIndex = 1}
-//     else if(slideIndex < 1){slideIndex = slides.length}
-//     for (i = 0; i < slides.length; i++) {
-//        slides[i].style.display = "none";
-//     }
-//     for (i = 0; i < dots.length; i++) {
-//         dots[i].className = dots[i].className.replace(" active", "");
-//
-//       }
-//         slides[slideIndex-1].style.display = "block";
-//         dots[slideIndex-1].className += " active";
-//     }
-//
-// function currentSlide(index) {
-//     if (index > slides.length) {index = 1}
-//     else if(index < 1){index = slides.length}
-//     for (i = 0; i < slides.length; i++) {
-//        slides[i].style.display = "none";
-//     }
-//     for (i = 0; i < dots.length; i++) {
-//         dots[i].className = dots[i].className.replace(" active", "");
-//       }
-//         slides[index-1].style.display = "block";
-//         dots[index-1].className += " active";
-//     }
-//
-// function showSlides() {
-//     var i;
-//     slides = document.getElementsByClassName("mywork-card");
-//     dots = document.getElementsByClassName("dot");
-//     for (i = 0; i < slides.length; i++) {
-//        slides[i].style.display = "none";
-//     }
-//     slideIndex++;
-//     if (slideIndex> slides.length) {slideIndex = 1}
-//     for (i = 0; i < dots.length; i++) {
-//         dots[i].className = dots[i].className.replace(" active", "");
-//     }
-//     slides[slideIndex-1].style.display = "block";
-//     dots[slideIndex-1].className += " active";
-//     setTimeout(showSlides, 4000); // Change image every 3 seconds
-// }
-//
-// function span(){
-//   var getSkill = document.getElementsByClassName("work-skill");
-//   var getspan = document.getElementsByTagName("span");
-//   for (var i=0; i<getSkill.length; i++){
-//     // getspan[i].style.color="#ca3e47";
-//     getSkill[i].style.hover
-//   }
-// }
 
 /* Find all slideshow containers */
 var slideshowContainers = document.getElementsByClassName("mywork-section-card");
@@ -96,18 +38,7 @@ function showSlides(slides, slideIndex, cycle) {
 };
 
 
-// function currentSlide(index) {
-//     if (index > slides.length) {index = 1}
-//     else if(index < 1){index = slides.length}
-//     // for (i = 0; i < slides.length; i++) {
-//     //    slides[i].style.display = "none";
-//     // }
-//     for (i = 0; i < dots.length; i++) {
-//         dots[i].className = dots[i].className.replace(" active", "");
-//       }
-//         // slides[index-1].style.display = "block";
-//         dots[index-1].className += " active";
-//     }
+
 
 
 var prevScrollpos = window.pageYOffset;
